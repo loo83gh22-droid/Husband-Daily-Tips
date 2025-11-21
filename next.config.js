81bd5426e002a673b23cfd261479499a12f3bea6
loo83gh22-droid <waterloo1983hawk22@gmail.com>
2025-11-20 23:52:45 -0800

    Add console error suppression for hydration warnings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Suppress hydration warnings in production
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
 }
 
 module.exports = nextConfig
